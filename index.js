094905da71858a79298a28253efb5d5763ad0b95
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-07-16 22:51:47 +0530

    modified inde.js

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const inputEl = document.getElementById("input-el");
 
 const button = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
+
 button.addEventListener("click", function () {
   // let inpuValue = document.getElementById("input-el").value
   // myLeads.push(inpuValue)
   myLeads.push(inputEl.value)
-  console.log("Button clicked!");
-  myLeads.push("www.myleads.com");
-  console.log(myLeads);
+  inputEl.value =""
+  renderLeads()
+ 
 });
 
+function renderLeads(){
+
+let listItems = ""
 for (let i=0; i<myLeads.length; i++){
-  ulEl.textContent += myLeads[i]
+  // listItems += "<li><a target ='_blank' href='" +myLeads[i] + "' >"+ myLeads[i] +"</a></li> "
+  listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`;
+
 }
+
+ulEl.innerHTML = listItems
+}
+
+
