da8ff8cb989c8f0aa8455ea3cdb7b3692ee9f78b
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-08-20 03:48:30 +0530

    added delete all button

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const inputBtn2 = document.getElementById("delete-btn");
+const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-localStorage.clear()
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
-console.log(leadsFromLocalStorage)
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    // Save the myLeads array to localStorage 
-    // PS: remember JSON.stringify()
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
-    
-    // To verify that it works:
-    console.log( localStorage.getItem("myLeads") )
-})
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  // Save the myLeads array to localStorage
+  // PS: remember JSON.stringify()
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+
+  // To verify that it works:
+  console.log(localStorage.getItem("myLeads"));
+});
+
+inputBtn2.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
